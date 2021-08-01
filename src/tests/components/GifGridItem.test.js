957a7f8ce0fbb94bdb3ada5e0e1f9769a744243a
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-01 04:12:03 -0400

    Generando el propTypes required para el titulo y la url en el Componente

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -8,12 +8,9 @@ describe('Pruebas en <GifGridItem/> Component', () => {
 
 
     test('Debe hacer algo', ()=>{
-        const image = {
-            id:1,
-            title: 'Hola soy Goku',
-            url: 'https://p325k7wa.twic.pics/high/dragon-ball/dragonball-project-z/00-page-setup/dbzk_banner_final.jpg?twic=v1/dpr=2/cover=4060x1380/step=10/quality=80'
-        }
-        const wrapper = shallow(<GifGridItem image={image}/>); //simular click o tener el documto como queremos 
+        const  title = 'Hola soy Goku';
+        const url = 'https://p325k7wa.twic.pics/high/dragon-ball/dragonball-project-z/00-page-setup/dbzk_banner_final.jpg?twic=v1/dpr=2/cover=4060x1380/step=10/quality=80'
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>); //simular click o tener el documto como queremos 
         expect(wrapper).toMatchSnapshot(); //Tira la foto
     })
 
