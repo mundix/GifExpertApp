341942316e8574f397cad50e5991f61671d1f35d
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-01 03:56:15 -0400

    Completed Ensyme and Enzyme to json setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createSerializer } from 'enzyme-to-json'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { createSerializer } from 'enzyme-to-json';
 // import '@testing-library/jest-dom/extend-expect';
 
 Enzyme.configure({ adapter: new Adapter() });
