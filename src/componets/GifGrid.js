56618596b236ea9a5e0213a8a68b6179e75b1616
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-04 03:44:52 -0400

    prueba del componte GifGrid with Mock

--- src/componets/GifGrid.js
+++ src/componets/GifGrid.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category}) => {
@@ -21,4 +22,8 @@ export const GifGrid = ( {category}) => {
             </div>
         </>
     )
+}
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired
 }
---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGrid } from '../../componets/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+//Si hago esto en cada test debo usar esta funcion falsead
+jest.mock('../../hooks/useFetchGifs'); //esto ayuda a usar el useFetch y controlar la informacion quee sto va a responder 
+
+
+describe('Pruebas en el archivo <GifGrid/>', () => {
+
+    const category = 'Batman';
+
+    test('Mostrando contenido', () => {
+      
+          useFetchGifs.mockReturnValue({
+            data: [],
+             loading: true
+          });
+
+        const wrapper =  shallow(<GifGrid category={category}/>);
+        expect(wrapper).toMatchSnapshot();
+        
+    })
+    
+    //probar mi useFetch que debe mostar item cuanod se cargan imagenes 
+    test('Debe de mostar items cuando se carga imagenes useFetch', async () => {
+        //probar de mi custom hook regreso la informacion cuando ya la cargo. 
+        // useFetchGifs
+        const gifs = [
+            {
+              id: "ABC",
+              url: "https://localhost/img.jpg",
+              title: "Title",
+            },
+          ];
+      
+          useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false,
+          });
+
+        const wrapper =  shallow(<GifGrid category={category}/>);
+
+        expect(wrapper).toMatchSnapshot();
+
+    })
+    
+
+});
